fcbc97be9d2df4894e7bb43508bc3d0eeec31940
vibhujuneja08-hash <vibhujuneja08@gmail.com>
2026-07-24 22:31:32 +0530

    Reverse Integer

---
+++ leetcode/editor/en/[7]Reverse Integer.java
@@ -0,0 +1,73 @@
+//Given a signed 32-bit integer x, return x with its digits reversed. If 
+//reversing x causes the value to go outside the signed 32-bit integer range [-2³¹, 2³¹ -
+// 1], then return 0. 
+//
+// Assume the environment does not allow you to store 64-bit integers (signed 
+//or unsigned). 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: x = 123
+//Output: 321
+// 
+//
+// Example 2: 
+//
+// 
+//Input: x = -123
+//Output: -321
+// 
+//
+// Example 3: 
+//
+// 
+//Input: x = 120
+//Output: 21
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// -2³¹ <= x <= 2³¹ - 1 
+// 
+//
+// Related Topics Math 👍 15702 👎 14023
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int reverse(int x) {
+
+        int reverse = 0;
+
+        while (x != 0) {
+
+
+            int digit = x % 10;
+
+
+            if (reverse > Integer.MAX_VALUE / 10 ||
+                    (reverse == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+
+
+            if (reverse < Integer.MIN_VALUE / 10 ||
+                    (reverse == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+
+            reverse = reverse * 10 + digit;
+
+
+            x = x / 10;
+        }
+
+        return reverse;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
