cc8091fbcaeae5ef44fa0d4f7e5395c2acb81212
vibhujuneja08-hash <vibhujuneja08@gmail.com>
2026-08-02 05:48:18 +0530

    Four Sum

---
+++ leetcode/editor/en/[18]4Sum.java
@@ -0,0 +1,81 @@
+//Given an array nums of n integers, return an array of all the unique 
+//quadruplets [nums[a], nums[b], nums[c], nums[d]] such that: 
+//
+// 
+// 0 <= a, b, c, d < n 
+// a, b, c, and d are distinct. 
+// nums[a] + nums[b] + nums[c] + nums[d] == target 
+// 
+//
+// You may return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,0,-1,0,-2,2], target = 0
+//Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [2,2,2,2,2], target = 8
+//Output: [[2,2,2,2]]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 200 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 
+//
+// Related Topics Array Two Pointers Sorting 👍 13054 👎 1549
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if  (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int left = j + 1, right = nums.length - 1;
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        list.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    }
+                    else if (sum > target) {
+                        right--;
+                    }
+                    else if (sum < target) {
+                        left++;
+                    }
+                }
+
+
+            }
+        }
+        return list;
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
