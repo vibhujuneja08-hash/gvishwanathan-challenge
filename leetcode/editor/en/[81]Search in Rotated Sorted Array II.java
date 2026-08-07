6746ff8832504c944239085a20f654e59ef46cc2
vibhujuneja08-hash <vibhujuneja08@gmail.com>
2026-08-08 01:06:42 +0530

    Search in rotated array with duplicates

---
+++ leetcode/editor/en/[81]Search in Rotated Sorted Array II.java
@@ -0,0 +1,79 @@
+//There is an integer numsay nums sorted in non-decreasing order (not 
+//necessarily with distinct values). 
+//
+// Before being passed to your function, nums is rotated at an unknown pivot 
+//index k (0 <= k < nums.length) such that the resulting numsay is [nums[k], nums[k+1]
+//, ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0
+//,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,
+//2,4,4]. 
+//
+// Given the numsay nums after the rotation and an integer target, return true 
+//if target is in nums, or false if it is not in nums. 
+//
+// You must decrease the overall operation steps as much as possible. 
+//
+// 
+// Example 1: 
+// Input: nums = [2,5,6,0,0,1,2], target = 0
+//Output: true
+// 
+// Example 2: 
+// Input: nums = [2,5,6,0,0,1,2], target = 3
+//Output: false
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 5000 
+// -10⁴ <= nums[i] <= 10⁴ 
+// nums is guaranteed to be rotated at some pivot. 
+// -10⁴ <= target <= 10⁴ 
+// 
+//
+// 
+// Follow up: This problem is similar to Search in Rotated Sorted numsay, but 
+//nums may contain duplicates. Would this affect the runtime complexity? How and why?
+// 
+//
+// Related Topics numsay Binary Search 👍 9807 👎 1132
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int low=0;
+        int high =nums.length-1;
+        while (low<=high){
+            int mid =low +(high - low)/2;
+            if (nums[mid]== target ){
+                return true;
+            }
+            if (nums[low ]== nums[mid]&& nums [mid]==nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if (nums[low]<=nums[mid]){
+                if (nums[low]<=target&& target < nums[mid]){
+                    high =mid-1;
+                }
+                else{
+                    low=mid +1;
+                }
+            }
+            else {
+                if (nums[mid]<target&& target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high= mid-1;
+                }
+            }
+
+        }
+
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
